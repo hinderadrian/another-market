ff8d3930947228d1ab07b112ce47884acae814ff
Hinder Adrian Alvarez Perlaza <halvarezadrian@gmail.com>
2020-10-04 20:55:57 -0500

    The initial structure of the project is uploaded

---
+++ src/main/java/com/hinderadrian/market/AnotherMarketApplication.java
@@ -0,0 +1,13 @@
+package com.hinderadrian.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnotherMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnotherMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hinderadrian/market/AnotherMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hinderadrian.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnotherMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
